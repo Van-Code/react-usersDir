706e424a96f10c81040976c2170cb175d52fa9c8
Van-Code <Van-Code@users.noreply.github.com>
2020-05-26 14:38:03 -0400

    Updates

---
+++ precache-manifest.122b9397a076926368db7bda838b91aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7887b8cac3312d3ce3f1e5ba99965047",
+    "url": "/react-usersDir/index.html"
+  },
+  {
+    "revision": "39bb863e532e02ef461a",
+    "url": "/react-usersDir/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "39bb863e532e02ef461a",
+    "url": "/react-usersDir/static/js/2.64f59d05.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-usersDir/static/js/2.64f59d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e25bbb472f536ebdc5d",
+    "url": "/react-usersDir/static/js/main.c692bb32.chunk.js"
+  },
+  {
+    "revision": "9cecbc8b757897ef413c",
+    "url": "/react-usersDir/static/js/runtime-main.215762d1.js"
+  }
+]);
